ef1d5c1f6bc26fc034a8e1b7c91f20ce7e3c08f6
Mostafa ASHARF <masharf@sqli.com>
2021-01-06 22:49:55 +0100

    :hammer: Add {owner} attribute to {GitProjectsManager}

--- src/gpm.sh
+++ src/gpm.sh
@@ -4,7 +4,7 @@
 if [[ ! -f "${gitProjects}" ]]; then printf "" > "${gitProjects}"; fi
 
 githubFields=(alias username token owner repo vcs)
-gitlabFields=(alias username token projectId domainName repo vcs)
+gitlabFields=(alias username token projectId domainName owner repo vcs)
 
 getFormatedFieldValue() {
     fieldName=${1}
